ee8050aae1a0175e05f8e4baacca93b39d55fe06
Robert O'Brien <robrien20@gmail.com>
2023-03-22 21:35:36 -0400

    add dummy about page

--- pages/about.js
+++ pages/about.js
@@ -1,25 +1,30 @@
+// pages/about.js
 import React from "react";
 import Layout from "../components/Layout";
+import styles from "../styles/About.module.css";
 
 const About = () => (
   <Layout>
-    <h2>About Us</h2>
-    <p>
-      We are a company that provides the best services and products for our
-      customers. Our goal is to deliver exceptional value, exceed expectations,
-      and maintain the highest standards of quality and integrity.
-    </p>
-    <p>
-      Our team consists of industry experts with years of experience in their
-      respective fields. We are committed to continuous improvement and
-      innovation, and we strive to create a positive impact in our community and
-      beyond.
-    </p>
-    <p>
-      Thank you for visiting our website, and please feel free to contact us
-      with any questions or feedback you may have. We look forward to serving
-      you and helping you achieve your goals.
-    </p>
+    <div className={styles.container}>
+      <h2 className={styles.title}>About Us</h2>
+      <p className={styles.paragraph}>
+        We are a company that provides the best services and products for our
+        customers. Our goal is to deliver exceptional value, exceed
+        expectations, and maintain the highest standards of quality and
+        integrity.
+      </p>
+      <p className={styles.paragraph}>
+        Our team consists of industry experts with years of experience in their
+        respective fields. We are committed to continuous improvement and
+        innovation, and we strive to create a positive impact in our community
+        and beyond.
+      </p>
+      <p className={styles.paragraph}>
+        Thank you for visiting our website, and please feel free to contact us
+        with any questions or feedback you may have. We look forward to serving
+        you and helping you achieve your goals.
+      </p>
+    </div>
   </Layout>
 );
 
